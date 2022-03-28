6e589963975024d6ac0a78e78cf6ed849a2474ca
“yhongzicong <973138379@qq.com>
2022-03-28 10:16:17 +0800

    [research] record create

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -17,6 +17,7 @@
 package vm
 
 import (
+	"github.com/ethereum/go-ethereum/research"
 	"math/big"
 	"sync/atomic"
 	"time"
@@ -526,6 +527,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 // Create creates a new contract using code as deployment code.
 func (evm *EVM) Create(caller ContractRef, code []byte, gas uint64, value *big.Int) (ret []byte, contractAddr common.Address, leftOverGas uint64, err error) {
 	contractAddr = crypto.CreateAddress(caller.Address(), evm.StateDB.GetNonce(caller.Address()))
+	research.PutRWSet(evm.BlockIndex, evm.TxIndex, []byte("create"+"-"+caller.Address().Hex()+"-"+contractAddr.Hex()))
 	return evm.create(caller, &codeAndHash{code: code}, gas, value, contractAddr, CREATE)
 }
 
@@ -536,6 +538,7 @@ func (evm *EVM) Create(caller ContractRef, code []byte, gas uint64, value *big.I
 func (evm *EVM) Create2(caller ContractRef, code []byte, gas uint64, endowment *big.Int, salt *uint256.Int) (ret []byte, contractAddr common.Address, leftOverGas uint64, err error) {
 	codeAndHash := &codeAndHash{code: code}
 	contractAddr = crypto.CreateAddress2(caller.Address(), salt.Bytes32(), codeAndHash.Hash().Bytes())
+	research.PutRWSet(evm.BlockIndex, evm.TxIndex, []byte("create2"+"-"+caller.Address().Hex()+"-"+contractAddr.Hex()))
 	return evm.create(caller, codeAndHash, gas, endowment, contractAddr, CREATE2)
 }
 
